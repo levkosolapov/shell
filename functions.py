4868bebcf0108ceaa6bc12eccb1220ef7b08800f
levkosolapov <white1488trashzt,ecj,fr>
2019-04-01 22:00:45 +0100

    обработал большую часть issues

--- functions.py
+++ functions.py
@@ -2,51 +2,64 @@ import os
 import tools
 import shutil
 
+
 def ls():
-    # ls. Вывести список файлов и директорий для текущей директории.
+    """ls. Вывести список файлов и директорий для текущей директории."""
     print('    '.join(os.listdir(path=".")))
     pass
 
 
 def pwd():
-    # pwd. Вывести полный путь для текущей директории.
+    """pwd. Вывести полный путь для текущей директории."""
     print(os.getcwd())
     pass
 
 
+# def cd(arguments: list):
+#     """cd <путь>. Переход по относительному или абсолютному пути."""
+#     # print(cd.__name__, 'was just triggered')
+#
+#
+#     if len(arguments) > 2:
+#         print(arguments)
+#         print('cd: too many arguments')
+#
+#     elif arguments == []:
+#         print('cd: additional argument required')
+#
+#     elif arguments == ['..']:
+#         tools.go_a_dir_up()
+#     else:
+#         path = arguments.pop()
+#         if os.path.exists(path):
+#             os.chdir(path)
+#         else:
+#             print('cd: No such file or directory')
 def cd(arguments: list):
-    # cd <путь>. Переход по относительному или абсолютному пути.
+    """cd <путь>. Переход по относительному или абсолютному пути."""
     # print(cd.__name__, 'was just triggered')
-
-
-    if len(arguments) > 2:
-        print(arguments)
-        print('cd: too many arguments')
-
-    elif arguments == []:
-        print('cd: additional argument required')
-
-    elif arguments == ['..']:
+    if arguments == ['..']:
         tools.go_a_dir_up()
     else:
-        path = arguments.pop()
-        if os.path.exists(path):
+        try:
+            path = arguments.pop()
             os.chdir(path)
-        else:
-            print('cd: No such file or directory')
+        except Exception as exc:
+            print(exc)
+
 
 
 def cp(arguments):
-    # cp <путь/имя файла>  <путь/имя файла>. Скопировать файл.
+    """cp <путь/имя файла>  <путь/имя файла>. Скопировать файл."""
 
     if len(arguments) > 2:
         print('cd: too many arguments')
     elif len(arguments) == 1:
         src = arguments.pop()
         if os.path.isfile(src):
-            #нужно доделать
+            # нужно доделать
             print('here should be complicated regex with copying')
-            #все
+            # все
         else:
             print('cp: No such file or directory')
     elif arguments == []:
@@ -62,7 +75,7 @@ def cp(arguments):
 
 
 def mv(arguments):
-    # mv <имя файла> <имя файла>. Переместить файл.
+    """ mv <имя файла> <имя файла>. Переместить файл """
 
     if len(arguments) > 2:
         print('mv: too many arguments')
@@ -78,8 +91,9 @@ def mv(arguments):
         except FileNotFoundError:
             print('cp: No such file or directory')
 
+
 def rm(arguments):
-    # rm <имя файла>. Удалить файл.
+    """ rm <имя файла>. Удалить файл."""
     if len(arguments) > 1:
         print('rm: too many arguments')
 
@@ -95,14 +109,14 @@ def rm(arguments):
         else:
             print('rm: No such file')
 
+
 def rmdir(arguments):
-    # rmdir <имя директории>. Удалить директорию в случае, если она пуста.
+    """rmdir <имя директории>. Удалить директорию в случае, если она пуста."""
 
     if len(arguments) > 1:
         print('rmdir: too many arguments')
-
-    elif len(arguments) == 0:
-        print('rmdir: additional argument required')
+    # elif len(arguments) == 0:
+    #     print('rmdir: additional argument required')
     else:
         name = arguments.pop()
         path = os.path.join(os.getcwd(), name)
@@ -116,7 +130,7 @@ def rmdir(arguments):
 
 
 def mkdir(arguments):
-    # mkdir <имя директории>. Создать директорию, если ее не было.
+    """mkdir <имя директории>. Создать директорию, если ее не было."""
 
     if len(arguments) > 1:
         print('mkdir: too many arguments')
@@ -129,6 +143,3 @@ def mkdir(arguments):
             os.mkdir(path)
         except FileExistsError:
             print('mkdir: folder already exists')
-
-
-
--- main.py
+++ main.py
@@ -1,43 +1,74 @@
-from functions import *
+import functions
+
+import inspect
 
 
-while True:
-    a = str(input())
+def main_func():
+    while True:
+        a = input()
 
-    # here we begin to separate command, flags and arguments
-    a = a.split()
-    command = a.pop(0)
-    flags = []
-    arguments = a.copy()
-    for word in a:
-        if word.startswith('-', 0, 1):
-            flags.append(word)
-            arguments.remove(word)
-    # here we end separation
-    if command in tools.commands_list:
-        if command == 'ls':
-            ls()
-        elif command == 'quit' or a == 'break':
-            print('bye bye, motherfucker!')
-            break
-        elif command == 'pwd':
-            pwd()
-        elif command == "cd":
-            cd(arguments)
-        elif command == 'cp':
-            cp(arguments)
-        elif command == 'mv':
-            mv(arguments)
-        elif command == 'rm':
-            rm(arguments)
-        elif command == 'rmdir':
-            rmdir(arguments)
-        elif command == 'mkdir':
-            mkdir(arguments)
-    else:
-        print('{}: No such command'.format(command))
+        # here we begin to separate command, flags and arguments
+        a = a.split()
+        command = a.pop(0)
+        flags = []
+        arguments = a.copy()
+        for word in a:
+            if word.startswith('-', 0, 1):
+                flags.append(word)
+                arguments.remove(word)
+        # here we end separation
+        try:
+            if command in commands_dict.keys():
+                if not arguments:
+                    commands_dict[command]()
+                else:
+                    commands_dict[command](arguments)
+            elif command == 'quit' or a == 'break':
+                print('bye bye, motherfucker!')
+                break
+            else:
+                print('{}: No such command'.format(command))
+        except Exception as err:
+            print(str(err))
 
 
+commands_dict = dict(inspect.getmembers(functions, inspect.isfunction))
 
+if __name__ == "__main__":
+    main_func()
 
-
+# while True:
+#     a = str(input())
+#
+#     # here we begin to separate command, flags and arguments
+#     a = a.split()
+#     command = a.pop(0)
+#     flags = []
+#     arguments = a.copy()
+#     for word in a:
+#         if word.startswith('-', 0, 1):
+#             flags.append(word)
+#             arguments.remove(word)
+#     # here we end separation
+#     if command in tools.commands_list:
+#         if command == 'ls':
+#             ls()
+#         elif command == 'quit' or a == 'break':
+#             print('bye bye, motherfucker!')
+#             break
+#         elif command == 'pwd':
+#             pwd()
+#         elif command == "cd":
+#             cd(arguments)
+#         elif command == 'cp':
+#             cp(arguments)
+#         elif command == 'mv':
+#             mv(arguments)
+#         elif command == 'rm':
+#             rm(arguments)
+#         elif command == 'rmdir':
+#             rmdir(arguments)
+#         elif command == 'mkdir':
+#             mkdir(arguments)
+#     else:
+#         print('{}: No such command'.format(command))
